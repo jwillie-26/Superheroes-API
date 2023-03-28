3d534d98aba597f9098b23b6f7fd89bfaa900769
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-03-28 10:58:25 +0300

    added config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :hero_powers, only: [:create]
-  resources :powers,only:[:index,:show, :update]
-  resources :heros,only:[:index, :show]
+  resources :powers, only: [:index,:show,:update]
+  resources :heros, only: [:index, :show]
 end
