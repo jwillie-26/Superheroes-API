a9856a8551a02a12076f35dde34e29c7055e8e04
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-03-29 15:50:37 +0300

    Redone migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_163844) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_25_163410) do
   create_table "hero_powers", force: :cascade do |t|
+    t.string "strength"
+    t.integer "power_id", null: false
+    t.integer "hero_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "strength"
-    t.integer "power_id"
-    t.integer "hero_id"
+    t.index ["hero_id"], name: "index_hero_powers_on_hero_id"
+    t.index ["power_id"], name: "index_hero_powers_on_power_id"
   end
 
   create_table "heros", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.string "super_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "powers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "hero_powers", "heros"
+  add_foreign_key "hero_powers", "powers"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,46 @@
-puts "start seeding..."
-HEROES = ["Captain America", "Black Panther", "Groot", "Hawkeye","Black Widow", "The Winter Soldier"]
-POWERS = ["Energy blasts", "martial artist", "speed", "healing", "stamina", "agility"]
-STRENGTH = ['Strong', 'Weak', 'Average']
-# # heroes
-6.times do
-    Hero.create(name: Faker::Internet.username, super_name: HEROES.uniq.sample)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating heroes..."
+
+# Create 10 Heroes
+10.times do
+    # name and super_name
+    name = Faker::Games::Heroes.name
+    super_name = Faker::Games::Heroes.specialty
+    Hero.create(name: name, super_name: super_name)
 end
 
-# powers
-6.times do
-   Power.create(name: POWERS.uniq.sample, description: Faker::Lorem.sentence)
+puts "Done creating heroes..."
+
+# Create 20 Powers
+puts "Creating powers..."
+
+20.times do
+    # name and description
+    name = Faker::Superhero.power
+    description = Faker::Lorem.paragraph_by_chars(number: 25)
+    Power.create(name: name, description: description)
 end
 
-# heropowers 
-powers = Power.all
-heroes = Hero.all
-6.times do
-    power = powers.uniq.sample
-    hero = heroes.uniq.sample
-    HeroPower.create(power_id: power.id, hero_id: hero.id, strength: STRENGTH.uniq.sample)
+puts "Done creating powers..."
+
+# Create 30 HeroPowers
+puts "Creating hero powers..."
+
+30.times do
+    arr = ["Strong", "Weak", "Average"]
+
+    # strength, power_id, and hero_id
+    strength = arr.sample
+    power_id = rand(1..20)
+    hero_id = rand(1..10)
+    HeroPower.create(strength: strength, power_id: power_id, hero_id: hero_id)
 end
-puts "done seeding."
+
+puts "Done creating hero powers..."
